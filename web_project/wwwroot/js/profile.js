5d5b9a7bbe6c6a9b511ff589123af8baf9a1539b
Paul <paul.babiciu@gmail.com>
2023-12-05 01:16:25 +0200

    Revert "signup, login, profile and css modified"
    This reverts commit 54ab2a0bf8debd501527e09f65d0f4aaeecb7a90.

--- web_project/wwwroot/js/profile.js
+++ web_project/wwwroot/js/profile.js
@@ -7,14 +7,10 @@ document.getElementById("change").addEventListener("click", function () {
     }
 });
 
-/*var loadFile = function (event) {
+var loadFile = function (event) {
     var output = document.getElementById("output");
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function () {
         URL.revokeObjectURL(output.src);
     };
-};*/
-
-function redirectToHomePage() {
-    window.location.href = '/Home/MainPage';
-}
+};
